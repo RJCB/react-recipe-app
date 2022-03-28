49da59ecfa133597fa3a88b5c78e21e16c822d2d
RJCB <37097058+RJCB@users.noreply.github.com>
2022-03-27 23:55:33 -0400

    install icons package,add back to home link,css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 //icons
 import cutleryIcon from "../images/cutlery-icon.png";
 import recipeIcon from "../images/recipe-icon.png";
 const Header = () => {
     return (
         <header>
-            <img src={recipeIcon} alt="salad" />
-            <img src={cutleryIcon} alt="cutlery" />
+            <Link to="/">
+                <img src={recipeIcon} alt="salad" />
+                <img src={cutleryIcon} alt="cutlery" />
+            </Link>
         </header>
     )
 }
--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -4,7 +4,17 @@ import { useParams } from "react-router-dom";
 import { API_URL, API_KEY } from "../config";
 //axios
 import axios from 'axios';
+//icons
+import { BiTime } from 'react-icons/bi';
+import { AiFillLike } from 'react-icons/ai';
+import { GiForkKnifeSpoon } from 'react-icons/gi';
+
+import noImage from "../images/no_image.jpg";
+
+import { Link } from "react-router-dom";
+
 const RecipeDetails = () => {
+    // const componentDataReady = useRef(false);
     const { recipeId } = useParams();
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
@@ -21,24 +31,24 @@ const RecipeDetails = () => {
                     id: data.id,
                     aggregateLikes: data.aggregateLikes,
                     instructions: data.instructions,
-                    time: data.cookingMinutes,
+                    time: data.readyInMinutes,
                     ingredients: data.extendedIngredients,
-                    image: data.image,
+                    image: data.image ? data.image : noImage,
                     nutrition: data.nutrients,
                     servings: data.servings,
                     summary: data.summary,
                     title: data.title,
                     vegetarian: data.vegetarian
                 }
-                setLoading(true);
-                console.log(recipeSummary);
+                setLoading(false);
                 setRecipeDetails(recipeSummary);//create a custom object with what we need
+                // componentDataReady.current = true;
             } catch (error) {
+                setLoading(false);
                 setError(true);
             }
         }
         fetchRecipeById();
-        setLoading(false);
     }, [recipeId]);
 
     const toggleButtons = () => {
@@ -48,48 +58,33 @@ const RecipeDetails = () => {
     }
 
     const ingredientsElements = () => {
-        if (recipeId) {
-            const elements = recipeDetails.ingredients.map((ingredient) => {
-                return <li key={ingredient.id}>{ingredient.original}</li>
-            })
-            return elements;
-        }
+        const elements = recipeDetails.ingredients.map((ingredient, index) => {
+            return <li key={`${index}#${ingredient.id}`}>{ingredient.original}</li>
+        })
+        return elements;
     }
-    // const ingredientsElements = recipeDetails.ingredients.map((ingredient) => {
-    //     return <li key={ingredient.id}>{ingredient.original}</li>
-    // })
-
-    // const ingredientsElements = () => {
-    //     // let elements = [];
-    //     console.log(recipeDetails.ingredients);
-    //     for (const ingredient in recipeDetails.ingredients) {
-    //         return <p key={recipeDetails.ingredients[ingredient].id}>{recipeDetails.ingredients[ingredient].original}</p>;
-    //     }
-    //     {/* return elements; */ }
-    // }
-
-    // const instructionsElements = recipeDetails.ingredients.map((ingredient) => {
-    //     return <li key={ingredient.id}>{ingredient.original}</li>
-    // })
 
     return (
         <div className="main-container recipe-details">
+            <Link className="back-to-home" to="/">Back to search page</Link>
             <h1>Recipe</h1>
             <div className="details-container">
-                <img src={recipeDetails.image} alt={recipeDetails.title} />
-                <div>
+                <div className="details-image">
+                    <img src={recipeDetails.image} alt={recipeDetails.title} />
+                </div>
+                <div className="details-info">
                     <h2>{recipeDetails.title}</h2>
-                    <p>{recipeDetails.summary}</p>
-                    <ul>
-                        <li>Likes: {recipeDetails.aggregateLikes}</li>
-                        <li>Vegetarian: {recipeDetails.vegetarian}</li>
-                        <li>Ready in: {recipeDetails.time}</li>
-                        <li>Servings: {recipeDetails.servings}</li>
+                    <p dangerouslySetInnerHTML={{ __html: recipeDetails.summary }} />
+                    <ul className="details-brief-info">
+                        <li><span className="details-icon"><AiFillLike /></span><span>Likes:</span>{recipeDetails.aggregateLikes}</li>
+                        <li><span className="details-icon"><BiTime /></span><span>Ready in:</span>{recipeDetails.time}</li>
+                        <li><span className="details-icon"><GiForkKnifeSpoon /></span><span>Servings:</span>{recipeDetails.servings}</li>
+                        {recipeDetails.ingredients ? <li><span>Number of ingredients:</span>{recipeDetails.ingredients.length}</li> : null}
                     </ul>
                     <div>
-                        <button onClick={toggleButtons}>Instructions</button>
-                        <button onClick={toggleButtons}>Ingredients</button>
-                        {recipeDetails && showIngredients ? <div><ul>{ingredientsElements()}</ul></div> : <div dangerouslySetInnerHTML={{ __html: recipeDetails.instructions }} />}
+                        <button className={!showIngredients ? "active" : ''} onClick={showIngredients ? toggleButtons : null}>Instructions</button>
+                        <button className={showIngredients ? "active" : ''} onClick={!showIngredients ? toggleButtons : null}>Ingredients</button>
+                        {recipeDetails && showIngredients ? <div className="details-ingredients"><ul>{ingredientsElements()}</ul></div> : <div className="details-instructions" dangerouslySetInnerHTML={{ __html: recipeDetails.instructions }} />}
                     </div>
                 </div>
             </div>
